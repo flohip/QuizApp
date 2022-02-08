6e23a154ba97c664dde4e7876ce5724f3a50ab25
Florian Hippenstiel <florian.hippenstiel@web.de>
2022-02-08 13:02:59 +0100

    added card folder

---
+++ style/components/Card/Card.stories.js
@@ -0,0 +1,12 @@
+import DefaultHtml from "./Card.default.html";
+
+export default {
+  title: "Components/Card",
+};
+
+export const Default = () => DefaultHtml;
+
+// export const Error = () =>  /*html*/
+// `
+// test
+// `;
