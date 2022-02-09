2370d723cc04abc2a903e6a4247acabb0b5f5f34
Florian Hippenstiel <florian.hippenstiel@web.de>
2022-02-09 21:09:24 +0100

    added Card and Header Design

--- style/components/Card/Card.stories.js
+++ style/components/Card/Card.stories.js
@@ -1,12 +1,30 @@
-import DefaultHtml from "./Card.default.html";
+// import DefaultHtml from "./Card.default.html";
 
 export default {
   title: "Components/Card",
 };
 
-export const Default = () => DefaultHtml;
+// export const Default = () => DefaultHtml;
 
-// export const Error = () =>  /*html*/
-// `
-// test
-// `;
+export const Card = () =>
+  /*html*/
+  `
+<article class="Card">
+  <h2 class="Card_title">QUESTION:</h2>
+  <div class="Card_content">
+    <p class="Card_question">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nemo eum ea
+      totam! Iure obcaecati odit soluta sint quidem similique, ipsam debitis nihil
+      sit iusto, cum aliquid neque placeat quibusdam.
+    </p>
+    <button class="Card_show">SHOW ANSWER</button>
+    <div class="tags">
+      <div class="tag1">#animal</div>
+      <div class="tag2">#nature</div>
+      <div class="tag3">#africa</div>
+      <div class="tag4">#wildlife</div>
+    </div>
+  </div>
+</article>
+
+`;
