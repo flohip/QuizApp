030b7dc7a4774f35676129eb1cfc21c7236bbbc0
Florian Hippenstiel <florian.hippenstiel@web.de>
2022-02-10 16:55:49 +0100

    added footer

--- style/components/Card/Card.stories.js
+++ style/components/Card/Card.stories.js
@@ -10,6 +10,7 @@ export const Card = () =>
   /*html*/
   `
 <article class="Card">
+  <img src="/../../../img/Bookmark_unmarked.png" alt="bookmark logo">
   <h2 class="Card_title">QUESTION:</h2>
   <div class="Card_content">
     <p class="Card_question">
@@ -18,11 +19,11 @@ export const Card = () =>
       sit iusto, cum aliquid neque placeat quibusdam.
     </p>
     <button class="Card_show">SHOW ANSWER</button>
-    <div class="tags">
-      <div class="tag1">#animal</div>
-      <div class="tag2">#nature</div>
-      <div class="tag3">#africa</div>
-      <div class="tag4">#wildlife</div>
+    <div class="Card_tags">
+      <div class="Card_tag1">#animal</div>
+      <div class="Card_tag2">#nature</div>
+      <div class="Card_tag3">#africa</div>
+      <div class="Card_tag4">#wildlife</div>
     </div>
   </div>
 </article>
