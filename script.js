a58b363ccc43b175da23d61c13218255173c4ee5
Florian Hippenstiel <florian.hippenstiel@web.de>
2022-02-17 12:33:08 +0100

    added the textarea's 'remaining characters' count

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
+// Toggles Answer Button of the index.html
 const toggleAnswerButton = document.querySelector(
   '[data-js="toggleAnswerButton"]'
 );
 const answerText = document.querySelector('[data-js="showHiddenAnswer"]');
 
-toggleAnswerButton.addEventListener('click', () => {
+//Question Textarea of the create_question.html
+let questionInput = document.querySelector('[data-js="questionTextarea"]');
+let questionLog = document.querySelector('[data-js="questionTextareaCount"]');
+const txtMaxLength = ' / 240 characters remaining';
+
+//Answer Textarea of the create_question.html
+const answerInput = document.querySelector('[data-js="answerTextarea"]');
+const answerLog = document.getElementById('Answer_textarea_count');
+
+// Toggles Answer Button of the index.html
+toggleAnswerButton?.addEventListener('click', () => {
   answerText.classList.toggle('Card_answer-show');
-  console.log('Button was clicked.');
+});
+
+//Question Textarea of the create_question.html
+questionInput?.addEventListener('input', e => {
+  console.log('Aktuelles Zeichen: ' + e.data);
+  console.log(e.target.value.length);
+  questionLog.textContent = e.target.value.length + txtMaxLength;
+});
+
+//Answer Textarea of the create_question.html
+answerInput?.addEventListener('input', e => {
+  console.log('Aktuelles Zeichen: ' + e.data);
+  console.log(e.target.value.length);
+  answerLog.textContent = e.target.value.length + txtMaxLength;
 });
